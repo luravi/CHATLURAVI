66ff253c5fcd80dff27e577611d2846f809646e8
LURAVI <61315321+luravi@users.noreply.github.com>
2023-10-24 16:25:29 +0200

    luravichat.py

--- luravichat.py
+++ luravichat.py
@@ -101,4 +101,10 @@ st.markdown(iframe_code, unsafe_allow_html=True)
 
 
 # Información en la barra lateral
-st.sidebar.markdown("<h3 style='text-align: center; font-size: 20px; color: Red'>Luravi- 2023</h3>", unsafe_allow_html=True)
+image_url = "https://github.com/luravi/CHATLURAVI/blob/github/yo6.jpg"
+
+st.sidebar.markdown(
+    f"<h3 style='text-align: center; font-size: 20px; color: Red'>Luravi- 2023</h3>"
+    f"<img src='{image_url}' width='200'>", 
+    unsafe_allow_html=True
+)
